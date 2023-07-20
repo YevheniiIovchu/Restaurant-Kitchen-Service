2ab76969141374bd75fc35840dd557b8da4c49a8
Yevhenii Iovchu <persie.iovchu@gmail.com>
2023-07-20 15:38:06 +0300

    created models

--- restaurant/apps.py
+++ restaurant/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class RestaurantServiceConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'restaurant'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "restaurant"
--- restaurant_service/wsgi.py
+++ restaurant_service/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for restaurant1 project.
+WSGI config for restaurant_service project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'restaurant1.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "restaurant_service.settings")
 
 application = get_wsgi_application()
